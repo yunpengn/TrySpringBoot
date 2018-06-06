861d9ba2b977cad70b1d038f6a2343b2e4ed6c2c
Yunpeng Niu <yunpeng.niu@hello-technology.com>
2018-06-06 15:34:17 +0800

    Finish user controller

--- src/main/java/org/yunpeng/tryspringboot/controllers/UserController.java
+++ src/main/java/org/yunpeng/tryspringboot/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.yunpeng.tryspringboot.controllers;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +24,27 @@ public class UserController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public String postNewUser(@ModelAttribute User user) {
+    public String newUser(@ModelAttribute User user) {
         users.put(user.getId(), user);
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String updateUser(@PathVariable Long id, @ModelAttribute User user) {
+        User originUser = users.get(id);
+        originUser.setName(user.getName());
+        originUser.setAge(user.getAge());
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
         return "success";
     }
 }
